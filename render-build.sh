d25e52586c6f54061c5cff3ca014e50110f92312
Abu Bakar SEM <AbuBakarSEM@proton.me>
2025-07-24 20:29:06 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+apt update && apt install -y ffmpeg libgl1 libglib2.0-0
+
 mkdir -p experiments/pretrained_models
 wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.8/GFPGANv1.3.pth -P experiments/pretrained_models
 
-# تثبيت GFPGAN مباشرة من GitHub
+# ثبت التبعيات يدويًا عشان ما يخرب البناء
+pip install torch==2.5.1 torchvision
+pip install opencv-python-headless numpy flask
+pip install basicsr realesrgan
+pip install facexlib
 pip install git+https://github.com/TencentARC/GFPGAN.git
