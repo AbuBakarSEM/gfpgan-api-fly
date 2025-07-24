ad2c5c1f752582a1fd44114e85b872b7f2898fdb
Abu Bakar SEM <AbuBakarSEM@proton.me>
2025-07-24 20:19:16 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,5 +3,5 @@
 mkdir -p experiments/pretrained_models
 wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.8/GFPGANv1.3.pth -P experiments/pretrained_models
 
-# ثبّت gfpgan يدويًا من GitHub
+# تثبيت GFPGAN مباشرة من GitHub
 pip install git+https://github.com/TencentARC/GFPGAN.git
